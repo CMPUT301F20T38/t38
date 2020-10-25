7589b4b1c2eb13636278aab3669d80662d22eaf7
Jieyuan Bi <jieyuan1@ualberta.ca>
2020-10-25 16:56:43 -0600

    add basic firebase database code

--- code/app/src/main/java/com/example/booker/MainActivity.java
+++ code/app/src/main/java/com/example/booker/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.booker;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+    HashMap<String, String> data = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,35 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        db = FirebaseFirestore. getInstance ();
+        final CollectionReference collectionReference = db.collection("Users");
+        data.put("Province Name","123");
+        collectionReference
+                .document("aaa")
+                .set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+// These are a method which gets executed when the task is succeeded
+                        Log. d ("TAG", "Data has been added successfully!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+// These are a method which gets executed if there’s any problem
+                        Log. d ("TAG", "Data could not be added!" + e.toString());
+                    }
+                });
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
+                    FirebaseFirestoreException error) {
+            }
+        });
     }
+
 
 }
