bd9399dbb77725fa5e72dda045b0a94410c94002
Yuntai Zeng <yuntai1@ualberta.ca>
2020-11-05 16:06:17 -0700

    Search 1.3

--- code/app/src/main/java/com/example/booker/ui/borrow/BorrowFragment.java
+++ code/app/src/main/java/com/example/booker/ui/borrow/BorrowFragment.java
@@ -45,6 +45,8 @@ public class BorrowFragment extends Fragment {
     private FirebaseFirestore db;
     private String search_content;
     private String ownerusername;
+    private ArrayList<String> userids;
+    private List<Map<String, Object>> booklist;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle saveInstanceState) {
@@ -53,11 +55,9 @@ public class BorrowFragment extends Fragment {
         Log.e("Start","start");
 
         db = FirebaseFirestore.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        final String userId = user.getUid();
 
 
-        final ArrayList<String> userids = new ArrayList<String>();
+        userids = new ArrayList<String>();
 
         db.collection("User").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -72,6 +72,50 @@ public class BorrowFragment extends Fragment {
 
                     Log.e("adduser", "adddone");
 
+                    for (String uid : userids) {
+                        Log.e("forstart", "1");
+                        Log.e("user", uid);
+
+
+                        db.collection("User").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                ownerusername = task.getResult().getString("Name");
+                            }
+                        });
+
+
+                        db.collection("User").document(uid).collection("Lend")
+                                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                Log.e("addbook", "add");
+
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                    Log.e("book", document.getString("title"));
+
+                                    Map<String, Object> map = new HashMap<String, Object>();
+                                    map.put("author", document.getString("author"));
+                                    map.put("title", document.getString("title"));
+                                    map.put("ISBN", document.getString("isbn"));
+
+                                    map.put("owner", ownerusername);
+                                    map.put("status", document.getString("status"));
+
+
+                                    booklist.add(map);
+
+                                }
+
+                                searchAdapter = new SearchListViewAdapter(getContext(), booklist);
+                                searchList.setAdapter(searchAdapter);
+
+                                searchEditText.setText("");
+                            }
+                        });
+                    }
+
                 } else {
                     Log.d("Retrieve Data", "Fail");
                 }
@@ -85,13 +129,13 @@ public class BorrowFragment extends Fragment {
 
         searchEditText = (EditText) root.findViewById(R.id.search_content);
 
-
+        booklist = new ArrayList<Map<String, Object>>();
 
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final List<Map<String, Object>> booklist = new ArrayList<Map<String, Object>>();
+                final List<Map<String, Object>> booklist1 = new ArrayList<Map<String, Object>>();
 
                 search_content = searchEditText.getText().toString();
 
@@ -121,9 +165,10 @@ public class BorrowFragment extends Fragment {
                                     for (QueryDocumentSnapshot document : task.getResult()) {
 
                                         Log.e("book", document.getString("title"));
+
                                         String thisauthor =  document.getString("author");
                                         String thistitle = document.getString("title");
-                                        String thisISBN = document.getString("ISBN");
+                                        String thisISBN = document.getString("isbn");
                                         String thisstatus = document.getString("status");
 
                                         Map<String, Object> map = new HashMap<String, Object>();
@@ -135,15 +180,16 @@ public class BorrowFragment extends Fragment {
                                         map.put("status",document.getString("status"));
 
 
-                                        //ISBN not included yet
-                                        if (thisauthor.contains(search_content) || thistitle.contains(search_content)
-                                                && (thisstatus == "available" || thisstatus == "requested")){
-                                            booklist.add(map);
+
+
+                                        if ((thisauthor.contains(search_content) || thistitle.contains(search_content) || thisISBN.contains(search_content))
+                                                && (thisstatus.equals("avaliable")  || thisstatus.equals("requested"))){
+                                            booklist1.add(map);
                                         }
 
                                     }
 
-                                    searchAdapter = new SearchListViewAdapter(getContext(),booklist);
+                                    searchAdapter = new SearchListViewAdapter(getContext(),booklist1);
                                     searchList.setAdapter(searchAdapter);
 
                                     searchEditText.setText("");
@@ -159,5 +205,52 @@ public class BorrowFragment extends Fragment {
         return root;
     }
 
+    public void setbook(){
+        Log.e("setbook","start");
+
+        for (String uid : userids) {
+            Log.e("forstart", "1");
+            Log.e("user", uid);
+
+
+            db.collection("User").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    ownerusername = task.getResult().getString("Name");
+                }
+            });
+
+
+            db.collection("User").document(uid).collection("Lend")
+                    .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    Log.e("addbook", "add");
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                        Log.e("book", document.getString("title"));
+
+                        Map<String, Object> map = new HashMap<String, Object>();
+                        map.put("author", document.getString("author"));
+                        map.put("title", document.getString("title"));
+                        map.put("ISBN", document.getString("isbn"));
+
+                        map.put("owner", ownerusername);
+                        map.put("status", document.getString("status"));
+
+
+                        booklist.add(map);
+
+                    }
+
+                    searchAdapter = new SearchListViewAdapter(getContext(), booklist);
+                    searchList.setAdapter(searchAdapter);
+
+                    searchEditText.setText("");
+                }
+            });
+        }
+    }
     
 }
