adf719e0d482425f4c49f360123a3d48c3c0e921
YeeLin01 <ylin3@ualberta.ca>
2020-11-06 02:44:05 -0700

    Update ChangeProfile.java

--- code/app/src/main/java/com/example/booker/activities/ChangeProfile.java
+++ code/app/src/main/java/com/example/booker/activities/ChangeProfile.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -137,7 +138,7 @@ public class ChangeProfile extends AppCompatActivity {
 
         @Override
         public void afterTextChanged(Editable editable) {
-
+     
         }
     };
 }
