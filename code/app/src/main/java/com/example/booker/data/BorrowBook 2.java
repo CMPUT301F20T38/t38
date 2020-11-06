119eacc951770c50b299ea39c88af271ca12eb2e
Yulong5 <71418779+Yulong5@users.noreply.github.com>
2020-11-07 07:32:47 +0800

    Delete BorrowBook 2.java

--- code/app/src/main/java/com/example/booker/data/BorrowBook 2.java
+++
@@ -1,7 +0,0 @@
-package com.example.booker.data;
-
-public class BorrowBook extends Book {
-    public BorrowBook(String author, String title, String ISBN, String status, String owner, String borrower) {
-        super(author, title, ISBN, status, owner, borrower);
-    }
-}
