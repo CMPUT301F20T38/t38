b040e3d959254a6e152a186d5ae52dc59cc1ee58
Yulong Wu <yulong5@ualberta.ca>
2020-11-07 07:07:40 +0800

    javadoc

---
+++ code/app/src/main/java/com/example/booker/data/OwnerListViewAdapter 2.java
@@ -0,0 +1,66 @@
+package com.example.booker.data;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.booker.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Adapter that extends arrayadapter
+ * ArrayList book that maintaint the book class
+ * context that get current activity
+ */
+public class OwnerListViewAdapter extends ArrayAdapter<Book> {
+
+    private ArrayList<Book> books;
+    private Context context;
+
+    public OwnerListViewAdapter(Context context, ArrayList<Book> books){
+        super(context, 0, books);
+        this.context = context;
+        this.books = books;
+    }
+
+
+
+    // getView that inflate the view of each item in the arraylist
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View view = convertView;
+
+        if (view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.owner_listview, parent, false);
+        }
+
+        Book book = books.get(position);
+
+        TextView author = view.findViewById(R.id.owner_book_author);
+        TextView title = view.findViewById(R.id.owner_book_title);
+        TextView ISBN = view.findViewById(R.id.owner_book_ISBN);
+        TextView tag = view.findViewById(R.id.owner_borrower_tag);
+        TextView borrower = view.findViewById(R.id.owner_borrower_name);
+        TextView status = view.findViewById(R.id.owner_book_status);
+
+        author.setText(book.getAuthor());
+        title.setText(book.getTitle());
+        ISBN.setText(book.getISBN());
+        tag.setText("");
+        borrower.setText(book.getBorrower());
+        status.setText(book.getStatus());
+
+        return view;
+    }
+}
